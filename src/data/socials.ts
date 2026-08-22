09595b24ff2b01927b8fc8b945cb537dc9aad273
Chirag Gupta <piyushg.2648@gmail.com>
2026-08-22 22:34:04 +0530

    Update social media links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,25 +3,25 @@ import { SocialLink } from '../types/social';
 export const socials: SocialLink[] = [
   {
     name: 'GitHub',
-    url: 'https://github.com',
+    url: 'https://github.com/chirag-g210?tab=repositories',
     icon: 'Github',
     color: 'hover:text-gray-900 dark:hover:text-white'
   },
   {
     name: 'LinkedIn',
-    url: 'https://linkedin.com',
+    url: 'https://www.linkedin.com/in/chirag-g210/',
     icon: 'Linkedin',
     color: 'hover:text-blue-600'
   },
   {
     name: 'Twitter / X',
-    url: 'https://twitter.com',
+    url: 'https://x.com/home',
     icon: 'Twitter',
     color: 'hover:text-sky-500'
   },
   {
     name: 'Email',
-    url: 'mailto:chirag.gupta@example.com',
+    url: 'mailto:cg2102005@gmail.com',
     icon: 'Mail',
     color: 'hover:text-emerald-500'
   }
